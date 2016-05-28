b619d24cf383fff36efb48d67e8dc39916d03415
root <root@user-desktop.(none)>
2016-05-28 15:41:58 +0300

    add robot.cpp and robot.h

--- finalProject/Robot/robot.cpp
+++ finalProject/Robot/robot.cpp
@@ -5,3 +5,44 @@
  *      Author: user
  */
 
+#include "robot.h"
+
+void Robot::Init(char* ip, int port, bool bMotor)
+{
+	_PlayerClient = new PlayerClient(ip, port);
+	_PositionProxy = new Position2dProxy(_PlayerClient);
+	_LaserProxy = new LaserProxy(_PlayerClient);
+	_PositionProxy->SetMotorEnable(bMotor);
+	_PlayerClient->Read();
+	_PositionProxy->SetOdometry(startPoseX,startPoseY,startPoseYaw);
+}
+
+double Robot::getX()
+{
+	return X;
+}
+
+double Robot::getY()
+{
+	return Y;
+}
+
+double Robot::getYaw()
+{
+	return Yaw;
+}
+
+void Robot::setSpeed(double speed, double angularSpeed)
+{
+	_PositionProxy->SetSpeed(speed,angularSpeed);
+}
+
+/*void Robot::setDirection(double Yaw)
+{
+	_PositionProxy->get
+}*/
+
+void Robot::Read()
+{
+	_playerClient->Read();
+}
--- finalProject/Robot/robot.h
+++ finalProject/Robot/robot.h
@@ -8,5 +8,41 @@
 #ifndef ROBOT_H_
 #define ROBOT_H_
 
+#include <libplayerc++/playerc++.h>
+//#include "../Utils/Utils.h"
+#include <vector>
+
+
+using namespace std;
+using namespace PlayerCc;
+
+class Robot {
+
+	PlayerClient* _PlayerClient;
+	Position2dProxy* _PositionProxy;
+	LaserProxy* _LaserProxy;
+	double X;
+	double Y;
+	double Yaw;
+	double startYaw;
+
+public:
+	double startPoseX;
+	double startPoseY;
+	double startPoseYaw;
+	double goalPoseX;
+	double goalPoseY;
+	double robotWidth;
+	double robotHeight;
+	void Init(char* ip, int port, bool bMotor);
+	double getX();
+	double getY();
+	double getYaw();
+	void setSpeed(double speed, double angularSpeed);
+	void Read();
+	//void setDirection(double Yaw);
+
+
+};
 
 #endif /* ROBOT_H_ */
